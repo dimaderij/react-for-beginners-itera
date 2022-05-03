77a074dcdc71c7d1b1bbaadb760addc008e27b81
dimaderij <dimaderij@gmail.com>
2022-05-03 13:18:57 +0300

    final

--- my-page/src/App.tsx
+++ my-page/src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {default as myinfo} from './aboutme.json';
+import image from './img/image.jpg';
+
+function ShowHeader() {
+  return (
+    <div className='Header'>Hi, i'm {myinfo.short_name} {myinfo.last_name}</div>
+  )
+}
+
+function ShowInfo() {
+  return (
+    <div>
+      <table className='Info'>
+        <tr>
+          <td>Age:</td>
+          <td>{myinfo.age}</td>
+          <td>Phone:</td>
+          <td><a href='tel:{myinfo.contact.phone}'>{myinfo.contact.phone}</a></td>
+        </tr>
+        <tr>
+          <td>Address:</td>
+          <td>{myinfo.more.address}; {myinfo.more.city}; {myinfo.more.country}</td>
+          <td>Mail:</td>
+          <td><a href='mailto:{myinfo.contact.email}'>{myinfo.contact.email}</a></td>
+        </tr>
+      </table>  
+    </div>
+  )
+}
+
+function ShowText() {
+  return (
+    <div className='Text'>
+      {myinfo.more.text}
+    </div>
+  )
+}
+
+function ShowPhoto() {
+  return (
+    <img className='Photo' src={image} alt='image'></img>
+  )
+}
 
 function App() {
+  document.title = "My page";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='Container'>
+      <div className='row'>
+        <ShowPhoto />
+        <ShowHeader />
+        <ShowInfo /> 
+      </div>
+      <div className='row'>            
+        <ShowText />
+      </div>
+    </div>  
+  )
+} 
 
 export default App;
